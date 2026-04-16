c6b185102d3468edd3d971df38379cbb2cd10373
joel-silva-design <joel09.silva.js@gmail.com>
2026-04-16 16:58:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const botaoBusca = document.getElementById("svg-busca");
+const inputBusca = document.getElementById("input-busca");
+
+botaoBusca.addEventListener('click', () =>{
+    inputBusca.style.visibility = "visible";
+});
